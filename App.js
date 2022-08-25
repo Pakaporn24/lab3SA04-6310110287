d6ff7b81f95e1d8e7626e9e0a848e7891fb3c414
Pakaporn24 <110577837+Pakaporn24@users.noreply.github.com>
2022-08-25 21:06:47 +0700

    Passing Props

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Weather from './components/Weather';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+
+export default function App() { 
+  const doIt = () => {
+    console.log("Hello from console")
+  }
+return (
+        <View style={styles.container}>
+           <Weather zipCode="90110"/>
+          <StatusBar style="auto" />
+      </View>
+);
+ }
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +22,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }, 
 });
