adeb19c7f544f5447ed5bbece9bd81ce94239143
Pakaporn24 <110577837+Pakaporn24@users.noreply.github.com>
2022-08-26 00:27:43 +0700

    Part 4

--- components/Weather.js
+++ components/Weather.js
@@ -1,15 +1,34 @@
-import React, { useState } from 'react'
-import { ImageBackground, StyleSheet, Text, View } from 'react-native'
-import Forecast from './Forecast'
-
-
+import React, { useEffect, useState } from "react";
+ import { ImageBackground, Text, StyleSheet } from 'react-native';
+ import { View } from "react-native";
+ import Forecast from "./Forecast";
+ import Constants from 'expo-constants';
+ 
 
 export default function Weather(props){
     const [forecastInfo, setForecastInfo] = useState({
-        main: '' ,
-        description: '',
+        main: '-',
+        description: '-',
         temp: 0
-    })
+    }
+)
+
+        useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=2cda5deca1b18916a3b6c153e8823f86`) 
+    .then((response) => response.json())
+    .then((json) => {
+                    setForecastInfo({
+    main: json.weather[0].main,
+                    description: json.weather[0].description,
+                     temp: json.main.temp
+    }); })
+                .catch((error) => {
+                console.warn(error);
+    }); }
+    }, [props.zipCode]) 
+
 
     return(
         <ImageBackground source={require('../gg.jpg')} style={style.backdrop}>
